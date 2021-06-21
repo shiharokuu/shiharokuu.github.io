0ff9f60a5006b79c7cb0082d2def533a8eb81cf5
shiharokuu <62873007+shiharokuu@users.noreply.github.com>
2021-06-21 13:57:25 -0600

    Add files via upload

---
+++ js/takeshiStats.js
@@ -0,0 +1,71 @@
+	
+stats = [{
+			"name": "Salud",
+			"value": "1",
+			"category": "Vida y Energía"
+		},
+		{
+			"name": "Juryoku",
+			"value": "1",
+			"category": "Vida y Energía"
+		},
+		{
+			"name": "Fuerza",
+			"value": "1",
+			"category": "Físico"
+		},
+		{
+			"name": "Defensa",
+			"value": "1",
+			"category": "Físico"
+		},
+		{
+			"name": "Poder",
+			"value": "1",
+			"category": "Mágico"
+		},
+		{
+			"name": "Resistencia Mágica",
+			"value": "1",
+			"category": "Mágico"
+		},
+		{
+			"name": "Velocidad",
+			"value": "1",
+			"category": "Otros"
+		},
+		{
+			"name": "Puntitos",
+			"value": "1",
+			"category": "Otros"
+		}
+	
+	];
+
+skill = [
+		{
+			"name": "Técnica 1: ???",
+			"shortName": "T1: ???",
+			"effect": "efecto#1 N/A",
+			"desc": "descripción#1 N/A."
+		},
+		{
+			"name": "Técnica 2: ???",
+			"shortName": "T2: ???",
+			"effect": "efecto#2 N/A",
+			"desc": "descripción#2 N/A."
+		},		{
+			"name": "Técnica 3: ???",
+			"shortName": "T3: ???",
+			"effect": "efecto#3 N/A",
+			"desc": "descripción#3 N/A."
+		},
+		{
+			"name": "Técnica 4: ???",
+			"shortName": "T4: ???",
+			"effect": "efecto#4 N/A",
+			"desc": "descripción#4 N/A."
+		}
+	];
+
+
