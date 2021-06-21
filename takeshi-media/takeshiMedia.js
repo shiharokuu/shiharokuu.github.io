4e3973d1a1f142626f35b1d7afc452499185a4f3
shiharokuu <62873007+shiharokuu@users.noreply.github.com>
2021-06-21 13:56:09 -0600

    Add files via upload

---
+++ takeshi-media/takeshiMedia.js
@@ -0,0 +1,45 @@
+	var mp3 =[
+      'takeshi-media/music/KisekiIV.mp3', 
+	  'takeshi-media/music/HajimariKiseki.mp3',
+	  'takeshi-media/music/MourningRefrain.mp3',
+	  'takeshi-media/music/StandUp.mp3',
+	  'takeshi-media/music/WayofLife.mp3',
+	  'takeshi-media/music/exorcist.mp3',
+	  'takeshi-media/music/barrier.mp3',
+	  'takeshi-media/music/DecisiveCollision.mp3'
+      ];
+
+      
+	var pjIMG =[
+      'takeshi-media/gifs/take01.gif',
+	  'takeshi-media/gifs/take02.gif',
+	  'takeshi-media/gifs/take03.gif',
+	  'takeshi-media/gifs/take04.gif',
+	  'takeshi-media/gifs/take05.gif',
+	  'takeshi-media/gifs/take06.gif',
+	  'takeshi-media/gifs/take07.gif',
+	  'takeshi-media/gifs/take08.gif',
+	  'takeshi-media/gifs/take09.gif',
+	  'takeshi-media/gifs/take10.gif',
+	  'takeshi-media/gifs/take11.gif',
+	  'takeshi-media/gifs/take12.gif',
+	  'takeshi-media/gifs/take13.gif',
+	  'takeshi-media/gifs/take14.gif',
+	  'takeshi-media/gifs/take15.gif',
+	  'takeshi-media/gifs/take16.gif',
+	  'takeshi-media/gifs/take17.gif'
+	  
+
+      ];
+      
+
+	var pjQuotes = [
+    "You trusted me, so I'll trust you too. That's what being friends means, doesn't it?",
+	"Don't you think that true memories are a combination of fun ones and painful ones?",
+	"Your past shouldn't stop you from achieving your goals and dreams",
+	"Don't just mindlessly judge people as you please",
+	"Time for Ul-tra-vio-lence",
+	" I can't afford to lose!"
+	
+
+  ];
