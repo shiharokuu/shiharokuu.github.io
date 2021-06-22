8382c0e49a0e1936710f3bc20ef7827c65cb6be6
shiharokuu <62873007+shiharokuu@users.noreply.github.com>
2021-06-22 11:57:18 -0600

    Update takeshiCode.js

--- js/takeshiCode.js
+++ js/takeshiCode.js
@@ -1,5 +1,7 @@
  //By Damian/Kenta/Shiharoku
 //¿Qué haces leyendo el código de mi firma? (?) Holi. 
+
+
 function openTabs(evt, tabName) {
   var i, tabcontent, tablinks;
   tabcontent = document.getElementsByClassName("tabcontent");
@@ -93,7 +95,7 @@ function rand( lowest, highest){
 		abilityText.appendChild(tabContent);
 		//Create H3 with ability name
 		var abilityName = document.createElement('h3');
-		abilityName.style.color = variant1;
+		//abilityName.style.color = variant1;
 		abilityName.innerHTML = i.name;
 		tabContent.appendChild(abilityName);
 		//Create p with ability description
@@ -102,7 +104,7 @@ function rand( lowest, highest){
 		tabContent.appendChild(abilityDesc);
 		//Create p with ability effect
 		var abilityEffect = document.createElement('p');
-		abilityEffect.style.color = variant2;
+		//abilityEffect.style.color = variant2;
 		abilityEffect.innerHTML = i.effect;
 		tabContent.appendChild(abilityEffect);
 	}
