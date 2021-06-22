8adae4d4317d43c96416e6934e87e48da6882450
shiharokuu <62873007+shiharokuu@users.noreply.github.com>
2021-06-22 10:53:17 -0600

    Update sheroCode.js

--- js/sheroCode.js
+++ js/sheroCode.js
@@ -1,4 +1,4 @@
- //By Damian/Kenta/Shiharoku
+ //By Damian/Kenta
 //¿Qué haces leyendo el código de mi firma? (?) Holi. 
 
 
@@ -15,7 +15,6 @@ function openTabs(evt, tabName) {
   document.getElementById(tabName).style.display = "block";
   evt.currentTarget.className += " active";
 }
-
 //Randomiza array
 function shuffleArray(array) {
     for (let i = array.length - 1; i > 0; i--) {
@@ -24,173 +23,137 @@ function shuffleArray(array) {
     }
 } 
 
-
-function rand( lowest, highest){
-    var adjustedHigh = (highest - lowest) + 1;       
-    return Math.floor(Math.random()*adjustedHigh) + parseFloat(lowest);
+//Calcula HP
+function getHP(vit, res) {
+  var base = 50;
+  return base + (vit*10)+(Math.round(res/2));   // The function returns the HP of my character
 }
 
-//Shuffle Traces
-	var trace1 = rand(0,mp3.length-1);
-	var trace2 = rand(0,pjQuotes.length-1);
-	var trace3 = rand(0,pjIMG.length-1);
 
-//HTML GENERATOR
-	const variant1 = "#8796b5";
-	const variant2 = "#8796b5"; 
-	const signature = document.getElementById('kenta');
-//Create Audio Tag
-	const audio = document.createElement('audio');
-	audio.setAttribute("src", mp3[trace1]);
-	audio.setAttribute("preload","auto");
-	signature.appendChild(audio);
-//Create IMG
-	const img = document.createElement('img');
-	img.classList.add('pjImage');
-	img.setAttribute("src", pjIMG[trace3]);
-	img.setAttribute("alt", "By Damian/Kenta/Shiharoku");
-	signature.appendChild(img);
+//Calcula Prana
+function getMP(prana, mana) {
+  var base = 40;
+  return base + (prana*10)+(Math.round(mana/2));   // The function returns the MP of my character
+}
 
-//Create TEXT
-	const textBlock = document.createElement('div');
-	textBlock.classList.add('pjText');
-	signature.appendChild(textBlock);
+//Calcula rango stat
+function rangoStat(stat) {
+  if(stat>40){
+	return "A"
+  }
+  else if(stat>30){
+	return "B"
+  }
+  else if(stat>20){
+	return "C"
+  }
+  else if(stat>10){
+	return "D" 
+  }
+  
+  return "E"
+  
+}
+	  
+//Stats de SHero, inicialziar
 
-//Create Ability
-	const ability = document.createElement('div');
-	ability.classList.add('pjHab');
-	ability.innerHTML = "Habilidades ";
-	textBlock.appendChild(ability);
-//Create Ability Text
-	const abilityText = document.createElement('div');
-	abilityText.classList.add('pjHabText');
-	//Create abilityTab
-	const abilityTab = document.createElement('div');
-	abilityTab.classList.add('tab');
-	//create deafaultButton
-	const buttonDefault = document.createElement('button');
-	buttonDefault.classList.add('tablinks');
+atk = 0
+vit = 0
+resF = 0
+resM = 0
+preci = 0
+ref = 0
+atkM = 0
+prana = 0
+prob = 0
+crit = 0
+np = 0
+	
+
+	  
+var xmlhttp = new XMLHttpRequest();
+xmlhttp.onreadystatechange = function() {
+
+  if (this.readyState == 4 && this.status == 200) {
+    var sheroPOWER = JSON.parse(this.responseText);
+  	atk = sheroPOWER.atk;
+	vit = sheroPOWER.vit;
+	resF = sheroPOWER.resF;
+	resM = sheroPOWER.resM;
+	res = resF + resM
+	preci = sheroPOWER.preci;
+	ref = sheroPOWER.ref;
+	atkM = sheroPOWER.atkM;
+	prana = sheroPOWER.prana;
+	mana = prana + atkM;
+	prob = sheroPOWER.prob;
+	crit = sheroPOWER.crit;
+	np = sheroPOWER.np;
+	skill = sheroPOWER.skill;
+	//console.log(skill[0].effect)
+	document.getElementById("hp").innerHTML = getHP(vit,res);
+	document.getElementById("mana").innerHTML = getMP(prana,mana);
+    document.getElementById("atk").innerHTML = atk;
+	document.getElementById("vit").innerHTML = vit;
+	document.getElementById("resF").innerHTML = resF;
+	document.getElementById("resM").innerHTML = resM;
+	document.getElementById("preci").innerHTML = preci;
+	document.getElementById("ref").innerHTML = ref;
+	document.getElementById("atkM").innerHTML = atkM;
+	document.getElementById("prana").innerHTML = prana;
+	document.getElementById("prob").innerHTML = prob;
+	document.getElementById("crit").innerHTML = crit;
+	document.getElementById("np").innerHTML = np;
+	document.getElementById("fuerza").innerHTML = rangoStat(atk+vit);
+	document.getElementById("resistencia").innerHTML = rangoStat(res);
+	document.getElementById("agilidad").innerHTML = rangoStat(preci+ref);
+	document.getElementById("manaRango").innerHTML = rangoStat(mana);
+	document.getElementById("criticoRango").innerHTML = rangoStat(prob+crit);
+    sheroSkillsName = "<div class='tab'>"
+	sheroSkillsDesc = ""
+	
     j = 0;
 	for (i of skill) {
 		if(j==0){
-			buttonDefault.setAttribute("onclick", "openTabs(event, '" + i.shortName + "')");
-			buttonDefault.innerHTML = i.shortName;
-			abilityTab.appendChild(buttonDefault);
+			sheroSkillsName = sheroSkillsName + "<button class=\"\ tablinks\"\ onclick=\"\openTabs(event, '" + i.shortName + "')\"\ id=\"\defaultOpen\"\>" + i.shortName + "</button>"
 			j+=1
 		}
 		else{
-			var buttonTab = document.createElement('button');
-			buttonTab.classList.add('tablinks');
-			buttonTab.setAttribute("onclick", "openTabs(event, '" + i.shortName  + "')");
-			buttonTab.innerHTML = i.shortName;
-			abilityTab.appendChild(buttonTab);
+			sheroSkillsName = sheroSkillsName + "<button class=\"\ tablinks\"\ onclick=\"\openTabs(event, '" + i.shortName + "')\"\>" + i.shortName + "</button>"
 		}
+		code = "<div id='" + i.shortName +  "' class='tabcontent'><h3 style='color:#e2664a'>" + i.name + "</h3>";
+		code = code + "<p>" + i.desc +"</p>";
+		code = code + "<p style='color:#fff3ab;'>" + i.effect +"</p></div>";
+		sheroSkillsDesc = sheroSkillsDesc + code;
 	}
-	abilityText.appendChild(abilityTab);
-	for (i of skill) {
-		//Create tabContent
-		var tabContent = document.createElement('div');
-		tabContent.classList.add('tabcontent');
-		tabContent.id = i.shortName;
-		abilityText.appendChild(tabContent);
-		//Create H3 with ability name
-		var abilityName = document.createElement('h3');
-		abilityName.style.color = variant1;
-		abilityName.innerHTML = i.name;
-		tabContent.appendChild(abilityName);
-		//Create p with ability description
-		var abilityDesc = document.createElement('p');
-		abilityDesc.innerHTML = i.desc;
-		tabContent.appendChild(abilityDesc);
-		//Create p with ability effect
-		var abilityEffect = document.createElement('p');
-		abilityEffect.style.color = variant2;
-		abilityEffect.innerHTML = i.effect;
-		tabContent.appendChild(abilityEffect);
-	}
-	ability.appendChild(abilityText);
+	sheroSkillsName = sheroSkillsName + "</div>";
+	sheroSkills = sheroSkillsName + sheroSkillsDesc;
+	document.getElementById("sheroSkills").innerHTML = sheroSkills;
 	// Get the element with id="defaultOpen" and click on it
-	buttonDefault.click();	
-//Create Quote
-	const quote = document.createElement('span');
-	quote.classList.add('pjQUOTE');
-	quote.innerHTML = '"' + pjQuotes[trace2] + '"';
-	textBlock.appendChild(quote);
+	document.getElementById("defaultOpen").click();
 	
-	
-//Create Signature Bottom
-	const sigBottom = document.createElement('div');
-	sigBottom.classList.add('sigBottom');
-	textBlock.appendChild(sigBottom);
-//Create HP BLOCK
-	const hpBlock = document.createElement('div');
-	hpBlock.classList.add('hpBlock');
-	sigBottom.appendChild(hpBlock);
-	//Create HP 
-		const hp = document.createElement('span');
-		hp.style.color = variant2;
-		hp.innerHTML = stats[0].name + ": ";
-		hpBlock.appendChild(hp);
-		const hpValue = document.createElement('span');
-		hpValue.innerHTML = stats[0].value;
-		hpBlock.appendChild(hpValue);
-//Create statTip
-	const statsTip = document.createElement('span');
-	statsTip.classList.add('statsTip');
-	statsTip.innerHTML = "<a href='https://jujutsukaisen-rol.foroactivo.com/t23-sensho-takeshi-id#43' target='_blank'><ruby><rb>戰勝 武士</rb><rp>（</rp><rt>Senshō Takeshi</rt><rp>）</rp></ruby></a>";
-	sigBottom.appendChild(statsTip);
-/*
-	const tableTip = document.createElement('table');
-	tableTip.classList.add('statsTiptext');
-	statsTip.appendChild(tableTip);
-	const bodyTip = document.createElement('tbody');
-	tableTip.appendChild(bodyTip);
-	for (let i =0;i<stats.length;i=i+2){
-		var tr = document.createElement('tr');
-		bodyTip.appendChild(tr);
-		tr.appendChild(stateMaker(i));
-		tr.appendChild(stateMaker(i+1));
+  }
+};
 
-	}
-	*/
-//Create HP BLOCK
-	const mpBlock = document.createElement('div');
-	mpBlock.classList.add('mpBlock');
-	sigBottom.appendChild(mpBlock);
-	//Create MP
-		const mp = document.createElement('span');
-		mp.classList.add('pjBars2');
-		mp.style.color = variant2;
-		mp.innerHTML = stats[1].name + ": ";
-		mpBlock.appendChild(mp);
-		const mpValue = document.createElement('span');
-		mpValue.innerHTML = stats[1].value;
-		mpBlock.appendChild(mpValue);
-		
+xmlhttp.open("GET", "https://shiharokuu.github.io/json/sheroStats.json", true);
+xmlhttp.send();
 
-
-	
-//Cargamos el reproductor
+ //Cargamos el reproductor
      audiojs.events.ready(function() {
         audiojs.createAll();
       });
 
-
-
-function stateMaker(i){
-	var td = document.createElement('td');
-	//State Name
-	var sName = document.createElement('span');
-	sName.style.color = variant2;
-	sName.innerHTML = stats[i].name + ": ";
-	td.appendChild(sName);
-	
-	//State Value
-	var sValue = document.createElement('span');
-	sValue.innerHTML = stats[i].value;
-	td.appendChild(sValue);
-	
-	return td;
-}
-
-
+    //shuffleArray(mp3);
+	var trace1 = Math.floor(Math.random() * mp3.length);
+    //shuffleArray(sheroQuotes);
+	var trace2 = Math.floor(Math.random() * sheroQuotes.length);
+    //shuffleArray(sheroIMG);
+	var trace3 = Math.floor(Math.random() * sheroIMG.length);
+	//var sheroMusic = '<audio  src="' + mp3[trace1] + '" preload="auto"></audio>'
+	// var imgCode ='<img src="'+ sheroIMG[trace3] + '" alt="Shero" class="sheroImage">';
+	var sheroSays = '"' + sheroQuotes[trace2] + '"';
+	var css = '<link rel="stylesheet" type="text/css" href="https://rawcdn.githack.com/shiharokuu/rpgstuff/8a09985069a73df17b4447698deaa13f6e7d994c/sheroStyle.css">'
+	//document.getElementById("sheroSignature").innerHTML=sheroMusic+imgCode+sheroSays+css;	
+	document.getElementById("sheroSays").innerHTML=sheroSays;	
+	document.getElementById("sheroGIF").src=sheroIMG[trace3];	
+	document.getElementById("sheroMusic").src=mp3[trace1];	
