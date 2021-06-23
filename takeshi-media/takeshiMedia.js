817378402e4216bafa7b97dcdf078df968162361
shiharokuu <62873007+shiharokuu@users.noreply.github.com>
2021-06-23 09:12:07 -0600

    Update takeshiMedia.js

--- takeshi-media/takeshiMedia.js
+++ takeshi-media/takeshiMedia.js
@@ -6,10 +6,11 @@
 	  'takeshi-media/music/WayofLife.mp3',
 	  'takeshi-media/music/exorcist.mp3',
 	  'takeshi-media/music/barrier.mp3',
-	  'takeshi-media/music/DecisiveCollision.mp3',
-	  'takeshi-media/music/BeyondtheJourney.mp3'
+	  'takeshi-media/music/DecisiveCollision.mp3'
       ];
-	var pjIMG =[
+/*
+	var pjIMG =
+	  [
       'takeshi-media/gifs/take01.gif',
 	  'takeshi-media/gifs/take02.gif',
 	  'takeshi-media/gifs/take03.gif',
@@ -27,14 +28,177 @@
 	  'takeshi-media/gifs/take15.gif',
 	  'takeshi-media/gifs/take16.gif',
 	  'takeshi-media/gifs/take17.gif'
+	  
+
       ];
-   
+*/      
+var pjIMG =[
+		//"You trusted me, so I'll trust you too. That's what being friends means, doesn't it?"
+		  [
+		  'takeshi-media/gifs/take01.gif',
+		  'takeshi-media/gifs/take02.gif',
+		   'takeshi-media/gifs/take03.gif',
+		  'takeshi-media/gifs/take07.gif',
+		  'takeshi-media/gifs/take08.gif',
+		  'takeshi-media/gifs/take09.gif',
+		  'takeshi-media/gifs/take16.gif',
+		  ],
+		//"I'm not fighting because I think I can win. I'm fighting because I have to win"
+		  [
+		  'takeshi-media/gifs/take02.gif',
+		  'takeshi-media/gifs/take06.gif',
+		  'takeshi-media/gifs/take07.gif',
+		  'takeshi-media/gifs/take10.gif',
+		  'takeshi-media/gifs/take13.gif',
+		  'takeshi-media/gifs/take14.gif',
+		  'takeshi-media/gifs/take16.gif',
+		  'takeshi-media/gifs/take17.gif'
+		  ],
+		//"Don't you think that true memories are a combination of fun ones and painful ones?"
+		  [
+		  'takeshi-media/gifs/take01.gif',
+		  'takeshi-media/gifs/take03.gif',
+		  'takeshi-media/gifs/take07.gif',
+		  'takeshi-media/gifs/take09.gif',
+		  'takeshi-media/gifs/take11.gif',
+		  'takeshi-media/gifs/take15.gif',
+		  'takeshi-media/gifs/take16.gif',
+		  ],
+		 //"Your past shouldn't stop you from achieving your goals and dreams"
+		  [
+		  'takeshi-media/gifs/take02.gif',
+		  'takeshi-media/gifs/take03.gif',
+		  'takeshi-media/gifs/take06.gif',
+		  'takeshi-media/gifs/take07.gif',
+		  'takeshi-media/gifs/take08.gif',
+		  'takeshi-media/gifs/take09.gif',
+		  'takeshi-media/gifs/take11.gif',
+		  'takeshi-media/gifs/take13.gif',
+		  'takeshi-media/gifs/take14.gif',
+		  'takeshi-media/gifs/take15.gif',
+		  ],
+		 //"Don't just mindlessly judge people as you please"
+		  [
+		  'takeshi-media/gifs/take02.gif',
+		  'takeshi-media/gifs/take03.gif',
+		  'takeshi-media/gifs/take06.gif',
+		  'takeshi-media/gifs/take07.gif',
+		  'takeshi-media/gifs/take10.gif',
+		  'takeshi-media/gifs/take13.gif',
+		  'takeshi-media/gifs/take14.gif',
+		  'takeshi-media/gifs/take15.gif',
+		  'takeshi-media/gifs/take16.gif',
+		  'takeshi-media/gifs/take17.gif'
+		  ],
+		 //"I can't afford to lose!"
+		  [
+		  'takeshi-media/gifs/take02.gif',
+		  'takeshi-media/gifs/take06.gif',
+		  'takeshi-media/gifs/take07.gif',
+		  'takeshi-media/gifs/take10.gif',
+		  'takeshi-media/gifs/take13.gif',
+		  'takeshi-media/gifs/take14.gif',
+		  'takeshi-media/gifs/take17.gif'
+		  ],
+		 //"Maybe this life is overrated... but I won't let the world burn around me"
+		  [
+		  'takeshi-media/gifs/take02.gif',
+		  'takeshi-media/gifs/take06.gif',
+		  'takeshi-media/gifs/take07.gif',
+		  'takeshi-media/gifs/take08.gif',
+		  'takeshi-media/gifs/take09.gif',
+		  'takeshi-media/gifs/take10.gif',
+		  'takeshi-media/gifs/take11.gif',
+		  'takeshi-media/gifs/take12.gif',
+		  'takeshi-media/gifs/take13.gif',
+		  'takeshi-media/gifs/take14.gif',
+		  'takeshi-media/gifs/take15.gif',
+		  'takeshi-media/gifs/take17.gif'
+		  ],
+		 //"Life is once in a time offer, so use it well"
+		  [
+		  'takeshi-media/gifs/take01.gif',
+		  'takeshi-media/gifs/take03.gif',
+		  'takeshi-media/gifs/take04.gif',
+		  'takeshi-media/gifs/take05.gif',
+		  'takeshi-media/gifs/take09.gif',
+		  'takeshi-media/gifs/take11.gif',
+		  'takeshi-media/gifs/take15.gif',
+		  ],
+		 //"Who the hell do you think I am?"
+		  [
+		  'takeshi-media/gifs/take02.gif',
+		  'takeshi-media/gifs/take03.gif',
+		  'takeshi-media/gifs/take06.gif',
+		  'takeshi-media/gifs/take07.gif',
+		  'takeshi-media/gifs/take10.gif',
+		  'takeshi-media/gifs/take13.gif',
+		  'takeshi-media/gifs/take14.gif',
+		  'takeshi-media/gifs/take17.gif'
+		  ],
+		 //"Go beyond the impossible and kick reason to the curb!"
+		  [
+		  'takeshi-media/gifs/take06.gif',
+		  'takeshi-media/gifs/take10.gif',
+		  'takeshi-media/gifs/take12.gif',
+		  'takeshi-media/gifs/take13.gif',
+		  'takeshi-media/gifs/take14.gif',
+		  'takeshi-media/gifs/take17.gif'
+		  ],
+		 //"I'm gonna show you... the power of the human race!"
+		  [
+		  'takeshi-media/gifs/take02.gif',
+		  'takeshi-media/gifs/take06.gif',
+		  'takeshi-media/gifs/take08.gif',
+		  'takeshi-media/gifs/take10.gif',
+		  'takeshi-media/gifs/take13.gif',
+		  'takeshi-media/gifs/take14.gif',
+		  'takeshi-media/gifs/take17.gif'
+		  ],
+		 //"When you're scared, that's all the more reason to move forward"
+		  [
+		  'takeshi-media/gifs/take01.gif',
+		  'takeshi-media/gifs/take02.gif',
+		  'takeshi-media/gifs/take06.gif',
+		  'takeshi-media/gifs/take07.gif',
+		  'takeshi-media/gifs/take08.gif',
+		  'takeshi-media/gifs/take09.gif',
+		  'takeshi-media/gifs/take11.gif',
+		  'takeshi-media/gifs/take12.gif',
+		  'takeshi-media/gifs/take13.gif',
+		  'takeshi-media/gifs/take14.gif',
+		  'takeshi-media/gifs/take15.gif',
+		  'takeshi-media/gifs/take17.gif'
+		  ],
+		 //"Let's see ya grit those teeth!"
+		  [
+		  'takeshi-media/gifs/take02.gif',
+		  'takeshi-media/gifs/take06.gif',
+		  'takeshi-media/gifs/take10.gif',
+		  'takeshi-media/gifs/take13.gif',
+		  'takeshi-media/gifs/take14.gif',
+		  'takeshi-media/gifs/take17.gif'
+		  ],
+		 //"A real man never dies, even when he's killed!"
+		  [
+		  'takeshi-media/gifs/take02.gif',
+		  'takeshi-media/gifs/take06.gif',
+		  'takeshi-media/gifs/take09.gif',
+		  'takeshi-media/gifs/take10.gif',
+		  'takeshi-media/gifs/take13.gif',
+		  'takeshi-media/gifs/take14.gif',
+		  ]	  
+	  ];
+
 	var pjQuotes = [
-        "You trusted me, so I'll trust you too. That's what being friends means, doesn't it?",
+    "You trusted me, so I'll trust you too. That's what being friends means, doesn't it?",
+	"I'm not fighting because I think I can win. I'm fighting because I have to win",
 	"Don't you think that true memories are a combination of fun ones and painful ones?",
 	"Your past shouldn't stop you from achieving your goals and dreams",
 	"Don't just mindlessly judge people as you please",
 	"I can't afford to lose!",
+	"Maybe this life is overrated... but I won't let the world burn around me",
+	"Life is once in a time offer, so use it well",
 	"Who the hell do you think I am?",
 	"Go beyond the impossible and kick reason to the curb!",
 	"I'm gonna show you... the power of the human race!",
