40be89e23ae06a4f8d015726941e89038d8ea664
shiharokuu <62873007+shiharokuu@users.noreply.github.com>
2021-08-10 17:58:59 -0600

    Add files via upload

---
+++ rpg/firmas/takeshi/pj-media/pjMedia.js
@@ -0,0 +1,272 @@
+/*Firma by Damian/Shiharoku/Kenta
+	Aquí se ponen los paths a las imagenes y música, además de las citas de personaje. 
+	Se pueden poner ciertas citas para solo ciertas imagenes, o simplemente dejarlo todo al azar.*/ 
+	var mp3 =[
+      'pj-media/music/KisekiIV.mp3', 
+	  'pj-media/music/HajimariKiseki.mp3',
+	  'pj-media/music/MourningRefrain.mp3',
+	  'pj-media/music/StandUp.mp3',
+	  'pj-media/music/hero.mp3',
+	  'pj-media/music/WayofLife.mp3',
+	  'pj-media/music/exorcist.mp3',
+	  'pj-media/music/barrier.mp3',
+	  'pj-media/music/DecisiveCollision.mp3',
+	  'pj-media/music/stepAhead.mp3'	
+      ];
+/*
+
+	var pjIMG =
+	  [
+      'pj-media/gifs/take01.gif',
+	  'pj-media/gifs/take02.gif',
+	  'pj-media/gifs/take03.gif',
+	  'pj-media/gifs/take04.gif',
+	  'pj-media/gifs/take05.gif',
+	  'pj-media/gifs/take06.gif',
+	  'pj-media/gifs/take07.gif',
+	  'pj-media/gifs/take08.gif',
+	  'pj-media/gifs/take09.gif',
+	  'pj-media/gifs/take10.gif',
+	  'pj-media/gifs/take11.gif',
+	  'pj-media/gifs/take12.gif',
+	  'pj-media/gifs/take13.gif',
+	  'pj-media/gifs/take14.gif',
+	  'pj-media/gifs/take15.gif',
+	  'pj-media/gifs/take16.gif',
+	  'pj-media/gifs/take17.gif'
+	  
+
+      ];
+*/      
+var pjIMG =[
+		//"You trusted me, so I'll trust you too. That's what being friends means, doesn't it?"
+		  [
+		  'pj-media/gifs/take01.gif',
+		  'pj-media/gifs/take02.gif',
+		  'pj-media/gifs/take03.gif',
+		  'pj-media/gifs/take07.gif',
+		  'pj-media/gifs/take08.gif',
+		  'pj-media/gifs/take09.gif',
+		  'pj-media/gifs/take16.gif',
+		  ],
+		//"I'm not fighting because I think I can win. I'm fighting because I have to win"
+		  [
+		  'pj-media/gifs/take02.gif',
+		  'pj-media/gifs/take06.gif',
+		  'pj-media/gifs/take07.gif',
+		  'pj-media/gifs/take10.gif',
+		  'pj-media/gifs/take13.gif',
+		  'pj-media/gifs/take14.gif',
+		  'pj-media/gifs/take17.gif'
+		  ],
+		//"Don't you think that true memories are a combination of fun ones and painful ones?"
+		  [
+		  'pj-media/gifs/take01.gif',
+		  'pj-media/gifs/take03.gif',
+		  'pj-media/gifs/take07.gif',
+		  'pj-media/gifs/take09.gif',
+		  'pj-media/gifs/take11.gif',
+		  'pj-media/gifs/take15.gif',
+		  'pj-media/gifs/take16.gif',
+		  ],
+		 //"Your past shouldn't stop you from achieving your goals and dreams"
+		  [
+		  'pj-media/gifs/take02.gif',
+		  'pj-media/gifs/take03.gif',
+		  'pj-media/gifs/take06.gif',
+		  'pj-media/gifs/take07.gif',
+		  'pj-media/gifs/take08.gif',
+		  'pj-media/gifs/take09.gif',
+		  'pj-media/gifs/take11.gif',
+		  'pj-media/gifs/take13.gif',
+		  'pj-media/gifs/take14.gif',
+		  'pj-media/gifs/take15.gif',
+		  ],
+		 //"Don't just mindlessly judge people as you please"
+		  [
+		  'pj-media/gifs/take02.gif',
+		  'pj-media/gifs/take03.gif',
+		  'pj-media/gifs/take06.gif',
+		  'pj-media/gifs/take07.gif',
+		  'pj-media/gifs/take10.gif',
+		  'pj-media/gifs/take13.gif',
+		  'pj-media/gifs/take14.gif',
+		  'pj-media/gifs/take15.gif',
+		  'pj-media/gifs/take16.gif',
+		  'pj-media/gifs/take17.gif'
+		  ],
+		 //"I can't afford to lose!"
+		  [
+		  'pj-media/gifs/take02.gif',
+		  'pj-media/gifs/take06.gif',
+		  'pj-media/gifs/take07.gif',
+		  'pj-media/gifs/take10.gif',
+		  'pj-media/gifs/take13.gif',
+		  'pj-media/gifs/take14.gif',
+		  'pj-media/gifs/take17.gif'
+		  ],
+		 //"Maybe this life is overrated... but I won't let the world burn around me"
+		  [
+		  'pj-media/gifs/take02.gif',
+		  'pj-media/gifs/take06.gif',
+		  'pj-media/gifs/take07.gif',
+		  'pj-media/gifs/take08.gif',
+		  'pj-media/gifs/take09.gif',
+		  'pj-media/gifs/take10.gif',
+		  'pj-media/gifs/take11.gif',
+		  'pj-media/gifs/take12.gif',
+		  'pj-media/gifs/take13.gif',
+		  'pj-media/gifs/take14.gif',
+		  'pj-media/gifs/take15.gif',
+		  'pj-media/gifs/take17.gif'
+		  ],
+		 //"Life is once in a time offer, so use it well"
+		  [
+		  'pj-media/gifs/take01.gif',
+		  'pj-media/gifs/take03.gif',
+		  'pj-media/gifs/take04.gif',
+		  'pj-media/gifs/take05.gif',
+		  'pj-media/gifs/take09.gif',
+		  'pj-media/gifs/take11.gif',
+		  'pj-media/gifs/take15.gif',
+		  ],
+		 //"Who the hell do you think I am?"
+		  [
+		  'pj-media/gifs/take02.gif',
+		  'pj-media/gifs/take03.gif',
+		  'pj-media/gifs/take06.gif',
+		  'pj-media/gifs/take07.gif',
+		  'pj-media/gifs/take10.gif',
+		  'pj-media/gifs/take13.gif',
+		  'pj-media/gifs/take14.gif',
+		  'pj-media/gifs/take17.gif'
+		  ],
+		 //"Go beyond the impossible and kick reason to the curb!"
+		  [
+		  'pj-media/gifs/take06.gif',
+		  'pj-media/gifs/take10.gif',
+		  'pj-media/gifs/take12.gif',
+		  'pj-media/gifs/take13.gif',
+		  'pj-media/gifs/take14.gif',
+		  'pj-media/gifs/take17.gif'
+		  ],
+		 //"I'm gonna show you... the power of the human race!"
+		  [
+		  'pj-media/gifs/take02.gif',
+		  'pj-media/gifs/take06.gif',
+		  'pj-media/gifs/take08.gif',
+		  'pj-media/gifs/take10.gif',
+		  'pj-media/gifs/take13.gif',
+		  'pj-media/gifs/take14.gif',
+		  'pj-media/gifs/take17.gif'
+		  ],
+		 //"When you're scared, that's all the more reason to move forward"
+		  [
+		  'pj-media/gifs/take01.gif',
+		  'pj-media/gifs/take02.gif',
+		  'pj-media/gifs/take06.gif',
+		  'pj-media/gifs/take07.gif',
+		  'pj-media/gifs/take08.gif',
+		  'pj-media/gifs/take09.gif',
+		  'pj-media/gifs/take11.gif',
+		  'pj-media/gifs/take12.gif',
+		  'pj-media/gifs/take13.gif',
+		  'pj-media/gifs/take14.gif',
+		  'pj-media/gifs/take15.gif',
+		  'pj-media/gifs/take17.gif'
+		  ],
+		 //"Let's see ya grit those teeth!"
+		  [
+		  'pj-media/gifs/take02.gif',
+		  'pj-media/gifs/take06.gif',
+		  'pj-media/gifs/take10.gif',
+		  'pj-media/gifs/take13.gif',
+		  'pj-media/gifs/take14.gif',
+		  'pj-media/gifs/take17.gif'
+		  ],
+		 //"A real man never dies, even when he's killed!"
+		  [
+		  'pj-media/gifs/take02.gif',
+		  'pj-media/gifs/take06.gif',
+		  'pj-media/gifs/take09.gif',
+		  'pj-media/gifs/take10.gif',
+		  'pj-media/gifs/take13.gif',
+		  'pj-media/gifs/take14.gif',
+		  ],
+		 //"What good will being scared of the dark do? What good will being scared of them do?"
+		  [
+		  'pj-media/gifs/take02.gif',
+		  'pj-media/gifs/take06.gif',
+		  'pj-media/gifs/take07.gif',
+		  'pj-media/gifs/take08.gif',
+		  'pj-media/gifs/take10.gif',
+		  'pj-media/gifs/take11.gif',
+		  'pj-media/gifs/take12.gif',
+		  'pj-media/gifs/take13.gif',
+		  'pj-media/gifs/take14.gif',
+		  'pj-media/gifs/take15.gif'
+		],
+		 //"If you wanna make progress, you can't just sit around!"
+		  [
+		  'pj-media/gifs/take01.gif',
+		  'pj-media/gifs/take02.gif',
+		  'pj-media/gifs/take06.gif',
+		  'pj-media/gifs/take07.gif',
+		  'pj-media/gifs/take08.gif',
+		  'pj-media/gifs/take10.gif',
+		  'pj-media/gifs/take13.gif',
+		  'pj-media/gifs/take14.gif',
+		  'pj-media/gifs/take17.gif'
+		],
+	
+		 //"Grit your teeth, and do your best to keep them safe"
+		  [
+		  'pj-media/gifs/take02.gif',
+		  'pj-media/gifs/take06.gif',
+		  'pj-media/gifs/take07.gif',
+		  'pj-media/gifs/take10.gif',
+		  'pj-media/gifs/take13.gif',
+		  'pj-media/gifs/take14.gif'
+		],		
+		 //"It's alright to fall, just pick yourself back up again"
+		  [
+		  'pj-media/gifs/take01.gif',
+		  'pj-media/gifs/take02.gif',
+		  'pj-media/gifs/take09.gif',
+		  'pj-media/gifs/take11.gif',
+		  'pj-media/gifs/take15.gif',
+		  'pj-media/gifs/take17.gif'
+		],
+		 //"It's alright to cry, just find your smile again"
+		  [
+		  'pj-media/gifs/take01.gif',
+		  'pj-media/gifs/take04.gif',
+		  'pj-media/gifs/take05.gif',
+		  'pj-media/gifs/take09.gif',
+		  'pj-media/gifs/take11.gif',
+		  'pj-media/gifs/take15.gif',
+		],
+		
+	  ];
+
+	var pjQuotes = [
+    	"You trusted me, so I'll trust you too. That's what being friends means, doesn't it?",
+	"I'm not fighting because I think I can win. I'm fighting because I have to win",
+	"Don't you think that true memories are a combination of fun ones and painful ones?",
+	"Your past shouldn't stop you from achieving your goals and dreams",
+	"Don't just mindlessly judge people as you please",
+	"I can't afford to lose!",
+	"Maybe this life is overrated... but I won't let the world burn around me",
+	"Life is once in a time offer, so use it well",
+	"Who the hell do you think I am?",
+	"Go beyond the impossible and kick reason to the curb!",
+	"I'm gonna show you... the power of the human race!",
+	"When you're scared, that's all the more reason to move forward",
+	"Let's see ya grit those teeth!",
+	"A real man never dies, even when he's killed!",
+	"What good will being scared of the dark do? What good will being scared of them do?",
+	"If you wanna make progress, you can't just sit around!",
+	"Grit your teeth, and do your best to keep them safe",
+	"It's alright to fall, just pick yourself back up again",
+	"It's alright to cry, just find your smile again",
+  ];
