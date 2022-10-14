5186a6a29a37839fc5586652a5132c88a82b7a7f
damiansh <damian221sh@hotmail.com>
2022-10-14 14:20:07 -0600

    billy

---
+++ rpg/firmas/billyBatson/js/pjStats.js
@@ -0,0 +1,71 @@
+/*Firma by Damian/Shiharoku/Kenta
+	En caso de querer usar stats, aquí se ponen, al igual que técnicas/skills.*/ 	
+stats = [{
+			"name": "HP",
+			"value": "1",
+			"category": "Vida y Energía"
+		},
+		{
+			"name": "CE",
+			"value": "1",
+			"category": "Vida y Energía"
+		},
+		{
+			"name": "Fuerza",
+			"value": "1",
+			"category": "Físico"
+		},
+		{
+			"name": "Defensa",
+			"value": "1",
+			"category": "Físico"
+		},
+		{
+			"name": "Poder",
+			"value": "1",
+			"category": "Mágico"
+		},
+		{
+			"name": "Resistencia Mágica",
+			"value": "1",
+			"category": "Mágico"
+		},
+		{
+			"name": "Velocidad",
+			"value": "1",
+			"category": "Otros"
+		},
+		{
+			"name": "Puntitos",
+			"value": "1",
+			"category": "Otros"
+		}
+	
+	];
+
+skill = [
+		{
+			"name": "<a target='_blank' href='https://dc-new-frontier.foroactivo.com/t361-welcome-to-mario-kart-bart-allen#1583'>Welcome to Mario Kart!</a>",
+			"shortName": "Issue #1",
+			"effect": "Guest-Starring: <a target='_blank' href='https://dc-new-frontier.foroactivo.com/u94' style='color:#1BA640'>Kid Flash (Bart Allen)</a>",
+			"desc": "Billy Batson, secretamente The World’s Mightiest Mortal, decide tomarse un tiempo libre y disfrutar un nuevo simulador usando Mario Kart en un arcade en San Francisco; sin esperarse encontrarse con Bart Allen, mejor conocido como Kid Flash, quién decide tomar una decisión de último segundo, para poder disfrutar del juego, claro, a expensas de Billy, literalmente. "
+		},
+		{
+			"name": "<a target='_blank' href='https://dc-new-frontier.foroactivo.com/t370-not-central-park-but-will-do-harley'>Not central park, but will do</a>",
+			"shortName": "Issue #2",
+			"effect": "Guest-Starring: <a target='_blank' href='https://dc-new-frontier.foroactivo.com/u2' style='color:#573c8f'>Harleen Quinzel</a>",
+			"desc": "En una colaboración con Whiz Radio, Billy trabaja de voluntariado en Gotham en un comedor. Tras finalizar la jornada y donarle algo de ropa a algunos jóvenes, Billy decide descansar en el parque, no sabiendo que puede que ahí termine encontrarse con cierta payasita. "
+		},		{
+			"name": "TBA",
+			"shortName": "Issue #3",
+			"effect": "TBA",
+			"desc": "TBA"
+		},
+		{
+			"name": "TBA",
+			"shortName": "Issue #4",
+			"effect": "TBA",
+			"desc": "TBA"
+		}
+	];
+
---
+++ rpg/firmas/billyBatson/pj-media/pjMedia.js
@@ -0,0 +1,120 @@
+/*Firma by Damian/Shiharoku/Kenta
+	Aquí se ponen los paths a las imagenes y música, además de las citas de personaje. 
+	Se pueden poner ciertas citas para solo ciertas imagenes, o simplemente dejarlo todo al azar.*/ 
+	var mp3 =[
+	  'pj-media/music/JustAKid.mp3',
+	  'pj-media/music/DontStopMeNow.mp3',
+	  'pj-media/music/YouSayRun.mp3',
+	  'pj-media/music/shazamTheme.mp3',
+	  'pj-media/music/thunderstruck.mp3',
+	  'pj-media/music/drill.mp3',
+	  'pj-media/music/gattai.mp3',
+	  'pj-media/music/rappu.mp3',
+	  'pj-media/music/libera.mp3',
+	  'pj-media/music/drill2.mp3'
+
+
+
+
+      ];
+/*
+
+	var pjIMG =
+	  [
+      'pj-media/gifs/take01.gif',
+	  'pj-media/gifs/take02.gif',
+	  'pj-media/gifs/take03.gif',
+	  'pj-media/gifs/take04.gif',
+	  'pj-media/gifs/take05.gif',
+	  'pj-media/gifs/take06.gif',
+	  'pj-media/gifs/take07.gif',
+	  'pj-media/gifs/take08.gif',
+	  'pj-media/gifs/take09.gif',
+	  'pj-media/gifs/take10.gif',
+	  'pj-media/gifs/take11.gif',
+	  'pj-media/gifs/take12.gif',
+	  'pj-media/gifs/take13.gif',
+	  'pj-media/gifs/take14.gif',
+	  'pj-media/gifs/take15.gif',
+	  'pj-media/gifs/take16.gif',
+	  'pj-media/gifs/take17.gif'
+	  
+
+      ];
+*/      
+var pjIMG =[
+		//1 "It's okay, Superman, I got your signal. It's going to be okay.", 
+
+		  [
+		  'pj-media/gifs/marvel1.png'
+		  ],
+		//2"Boy-o boy, there's nothing like the feel of the wind in your hair, cruising at mach speed."
+		  [
+		  'pj-media/gifs/marvel2.png'
+		  ],
+		//3"Forget your worries, your pressures...Forget you're late for school!"
+		  [
+		  'pj-media/gifs/billy1.gif'
+		  ],
+		 //4"It's silly worrying over things I have no control over"
+		  [
+		  'pj-media/gifs/marvel5.png',
+		   'pj-media/gifs/billy4.png'
+		  ],
+		 //5"When in doubt-- punch it out! That's my new motto!"
+		  [
+		  'pj-media/gifs/marvel6.gif'
+		  ],
+		 //6"Don't worry, I like to smile!"
+		  [
+		  'pj-media/gifs/marvel4.png',
+		  'pj-media/gifs/marvel5.png',
+		  'pj-media/gifs/billy3.gif'
+		  
+		  ],
+		 //7"Okay. I'm done being nice. All done."	
+		  [
+		  'pj-media/gifs/marvel3.png',
+		  'pj-media/gifs/marvel7.gif',
+		  'pj-media/gifs/marvel8.gif'
+		  ],
+		 //8"Brother, you are messing with the wrong god-power-wielding dude"
+		  [
+		  'pj-media/gifs/marvel6.gif',
+		  'pj-media/gifs/marvel8.gif'
+		  ],
+		 //9"Captain Marvel may have the courage of Achilles, but Billy Batson... has the courage of Billy Batson",
+		  [
+		  'pj-media/gifs/billy2.gif',
+		  ],
+		 //10"If he won't talk to Captain Marvel, maybe he'll open up to someone his own age"
+		  [
+		  'pj-media/gifs/billy5.png',
+		  ],
+		 //11"There are times when all of Marvel's powers cannot help me save a life, no matter how hard I try"
+		  [
+		  'pj-media/gifs/marvel9.png',
+		  ],
+		 //12"You have given them hope. It's a good and powerful force"
+		  [
+		  'pj-media/gifs/marvel10.png',
+		  ],
+	  ];
+
+	var pjQuotes = [
+	    "It's okay, Superman, I got your signal. It's going to be okay.",
+		"Boy-o boy, there's nothing like the feel of the wind in your hair, cruising at mach speed.",
+		"Forget your worries, your pressures...Forget you're late for school!",
+		"It's silly worrying over things I have no control over",
+		"When in doubt-- punch it out! That's my new motto!",
+		"Don't worry, I like to smile!",
+		"Okay. I'm done being nice. All done.",
+		"Brother, you are messing with the wrong god-power-wielding dude",
+		"Marvel may have the courage of Achilles, but Billy... has the courage of Billy Batson",
+		"If he won't talk to Captain Marvel, maybe he'll open up to someone his own age",
+		"There're times when Marvel's powers can't help me save a life, no matter how hard I try",
+		"You have given them hope. It's a good and powerful force",
+		
+
+		
+  ];
