fc99eb179b39f0b9286932e92bdfb0cf74b4eecd
shiharokuu <kenta_salvatore@hotmail.com>
2023-06-23 20:42:29 -0600

    Update pjStats.js
    update tema con Terra

--- rpg/firmas/billyBatson/js/pjStats.js
+++ rpg/firmas/billyBatson/js/pjStats.js
@@ -45,17 +45,18 @@ stats = [{
 
 skill = [
 		{
-			"name": "<a target='_blank' href='https://dc-new-frontier.foroactivo.com/t361-welcome-to-mario-kart-bart-allen#1583'>Welcome to Mario Kart!</a>",
+			"name": "<a target='_blank' href='https://dc-new-frontier.foroactivo.com/t638-tell-me-about-the-titans-tara#4119'>Tell me about the Titans</a>",
 			"shortName": "Issue #1",
-			"effect": "Guest-Starring: <a target='_blank' href='https://dc-new-frontier.foroactivo.com/u94' style='color:#1BA640'>Kid Flash (Bart Allen)</a>",
-			"desc": "Billy Batson, secretamente The World’s Mightiest Mortal, decide tomarse un tiempo libre y disfrutar un nuevo simulador usando Mario Kart en un arcade en San Francisco; sin esperarse encontrarse con Bart Allen, mejor conocido como Kid Flash, quién decide tomar una decisión de último segundo, para poder disfrutar del juego, claro, a expensas de Billy, literalmente. "
+			"effect": "Guest-Starring: <a target='_blank' href='https://dc-new-frontier.foroactivo.com/u148' style='color:#8d294d'>Terra</a>",
+			"desc": "Billy Batson, secretamente The World’s Mightiest Mortal, decide que es necesaro un cambio en su rutina y en su programa de radio, por lo que decide visitar San Francisco para entrevistar gente sobre los Titanes"
 		},
 		{
-			"name": "<a target='_blank' href='https://dc-new-frontier.foroactivo.com/t370-not-central-park-but-will-do-harley'>Not central park, but will do</a>",
+			"name": "TBA",			
 			"shortName": "Issue #2",
-			"effect": "Guest-Starring: <a target='_blank' href='https://dc-new-frontier.foroactivo.com/u2' style='color:#573c8f'>Harleen Quinzel</a>",
-			"desc": "En una colaboración con Whiz Radio, Billy trabaja de voluntariado en Gotham en un comedor. Tras finalizar la jornada y donarle algo de ropa a algunos jóvenes, Billy decide descansar en el parque, no sabiendo que puede que ahí termine encontrarse con cierta payasita. "
-		},		{
+			"effect": "TBA",
+			"desc": "TBA"
+		},		
+		{
 			"name": "TBA",
 			"shortName": "Issue #3",
 			"effect": "TBA",
