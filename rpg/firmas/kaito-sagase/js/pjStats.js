f7748513022c5c9a5a1a4e206df2746af95f7222
shiharokuu <kenta_salvatore@hotmail.com>
2024-08-17 13:11:18 -0600

    kaito firma y poké assets

---
+++ rpg/firmas/kaito-sagase/js/pjStats.js
@@ -0,0 +1,84 @@
+/*Firma by Damian/Shiharoku/Kenta
+	En caso de querer usar stats, aquí se ponen, al igual que técnicas/skills. */ 	
+stats = [{
+			"name": "HP",
+			"value": "1",
+			"category": "Vida y Energía"
+		},
+		{
+			"name": "CE",
+			"value": "1",
+			"category": "Vida y Energía"
+		},
+		{
+			"name": "Fuerza",
+			"value": "1",
+			"category": "Físico"
+		},
+		{
+			"name": "Defensa",
+			"value": "1",
+			"category": "Físico"
+		},
+		{
+			"name": "Poder",
+			"value": "1",
+			"category": "Mágico"
+		},
+		{
+			"name": "Resistencia Mágica",
+			"value": "1",
+			"category": "Mágico"
+		},
+		{
+			"name": "Velocidad",
+			"value": "1",
+			"category": "Otros"
+		},
+		{
+			"name": "Puntitos",
+			"value": "1",
+			"category": "Otros"
+		}
+	
+	];
+
+skill = [
+		{
+			"name": "<img src='../../pokemon-assets/pokemon/722.png'><br><img src='../../pokemon-assets/types/generation-vii/sun-moon/12.png'><img src='../../pokemon-assets/types/generation-vii/sun-moon/3.png'>",
+			"shortName": "Watson",
+			"effect": "<li>Impresionar</li><li>Hoja Afilada</li><li>Despejar</li><li>Rayo Confuso</li>",
+			"desc": "Espesura"
+		},
+		{
+			"name": "<img src='../../pokemon-assets/pokemon/92.png'><br><img src='../../pokemon-assets/types/generation-vii/sun-moon/8.png'><img src='../../pokemon-assets/types/generation-vii/sun-moon/4.png'>",		
+			"shortName": "Moriarty",
+			"effect": "TBA",
+			"desc": "TBA"
+		},		
+		{
+			"name": "Sprite",
+			"shortName": "Slot #3",
+			"effect": "Movements",
+			"desc": "Habilidad"
+		},
+		{
+			"name": "Sprite",
+			"shortName": "Slot #4",
+			"effect": "Movements",
+			"desc": "Habilidad"
+		},
+		{
+			"name": "Sprite",
+			"shortName": "Slot #5",
+			"effect": "Movements",
+			"desc": "Habilidad"
+		},
+		{
+			"name": "Sprite",
+			"shortName": "Slot #6",
+			"effect": "Movements",
+			"desc": "Habilidad"
+		}
+	];
+
